9c4710d4c466d3d2b956d7f67ad258eb3c70357a
Zainab Azzam <zainab_azzam@yahoo.com>
2020-05-13 04:36:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const y =['2'];
 function writeCards ( y){
   let z =[];
   for( let i =0 ; i< y.length;i++){
-    z.push(`Thank you, ${(y[i])} ,for the wonderful ${(y[i])} `);
+    z.push(`Thank you, ${(y[i])} ,for the wonderful ${(x)} `);
   }
   return z
 }
