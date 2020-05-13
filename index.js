b530e586aaa41e4623dbf27214c017c2be81b81d
Zainab Azzam <zainab_azzam@yahoo.com>
2020-05-13 04:34:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-let x =['1','hi']
-let y =['2']
-function writeCards (x, y){
-  let z=[]
-  for( let i =0 ; i< z.length;i++){
-    z.push(`Thank you, ${x[i]}, for the wonderful ${(y)} gift! `)
+const x =['1','hi','we'];
+const y =['2'];
+
+function writeCards ( y){
+  let z =[];
+  for( let i =0 ; i< y.length;i++){
+    z.push(`Thank you, ${(y)} ,for the wonderful ${(y)} `);
   }
   return z
 }
