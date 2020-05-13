4d5e718b6b77428a0a8455203b60c61f68a0189b
Zainab Azzam <zainab_azzam@yahoo.com>
2020-05-13 04:21:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+let x =['1','hi']
+let y =['2']
+function writeCards (x, y){
+  let z=[]
+  for( let i =0 ; i< z.length;i++){
+    z.push(`Thank you, ${x[i]}, for the wonderful ${(y)} gift! `)
+  }
+  return z
+}
